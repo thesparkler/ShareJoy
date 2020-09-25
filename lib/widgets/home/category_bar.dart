c786aafdf59ae691ad82917a41b897d22422e8a3
Siddhesh Tamhanekar <tamhanekar.siddhesh95@gmail.com>
2020-09-25 19:39:37 +0530

    setting screen added

--- lib/widgets/home/category_bar.dart
+++ lib/widgets/home/category_bar.dart
@@ -3,14 +3,22 @@ import 'package:Meme/providers/meme_provider.dart';
 import 'package:Meme/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:math' as Math;
 
-class CategoryBar extends StatelessWidget {
+class CategoryBar extends StatefulWidget {
   const CategoryBar({
     Key key,
     @required this.type,
   }) : super(key: key);
 
   final String type;
+
+  @override
+  _CategoryBarState createState() => _CategoryBarState();
+}
+
+class _CategoryBarState extends State<CategoryBar> {
+  bool showMore = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,17 +28,37 @@ class CategoryBar extends StatelessWidget {
         if (mp.categoryState == ViewState.loading)
           return CustomTheme.placeHolder;
 
-        final childs = mp.categories[type]
+        final childs = mp.categories[widget.type]
             .map<Widget>((e) => CategoryWidget(
                   category: e,
                   mp: mp,
                 ))
             .toList();
-        var shorts = childs.sublist(0, Math.min(8, childs.length));
+        var shortChilds = childs.sublist(
+            0, Math.min(showMore ? childs.length : 8, childs.length));
+        if (childs.length > 8) {
+          shortChilds.add(InkWell(
+            onTap: () {
+              setState(() {
+                showMore = !showMore;
+              });
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.white,
+              ),
+              child: showMore ? Text("less") : Text("more"),
+            ),
+          ));
+        }
         return Wrap(
           spacing: 2.0,
           alignment: WrapAlignment.center,
-          children: childs,
+          children: shortChilds,
         );
       },
     );
@@ -56,7 +84,11 @@ class CategoryWidget extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         margin: const EdgeInsets.all(4.0),
         decoration: BoxDecoration(
-          border: Border.all(color: Theme.of(context).primaryColor),
+          border: Border.all(
+            color: (mp.filters['category_id'] == category.id.toString())
+                ? Theme.of(context).primaryColor
+                : Colors.grey,
+          ),
           borderRadius: BorderRadius.circular(20.0),
           color: (mp.filters['category_id'] == category.id.toString())
               ? Theme.of(context).primaryColor
