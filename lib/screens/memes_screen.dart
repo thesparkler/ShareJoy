297e22a8f6f814488dee3b24cdc3e5a8f3e6a7dd
amitkumar3 <amitkumar@infiniteit.biz>
2020-09-27 14:09:24 +0530

    Added Nunito font

--- lib/screens/memes_screen.dart
+++ lib/screens/memes_screen.dart
@@ -33,10 +33,12 @@ class MemesScreen extends StatelessWidget {
                 backgroundColor: Colors.transparent,
                 elevation: 0,
                 title: Text(Config.titles[type],
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6
-                        .copyWith(fontSize: 24.0)),
+                    style: TextStyle(fontFamily: 'NunitoBold', fontSize: 20.0, color: Colors.black),
+                    // style: Theme.of(context)
+                    //     .textTheme
+                    //     .headline6
+                    //     .copyWith(fontSize: 20.0)
+                ),
               ),
               CategoryBar(type: type),
               PostList(type: type)
